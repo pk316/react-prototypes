f4994300abbb3d8ee49c737b40586ee54bd12ed0
pk316 <pr.khiev@gmail.com>
2017-10-19 19:28:58 -0700

    fundamental-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,33 @@
+import React from 'react';
+import Table from './table';
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>
+                Student Grade Table
+            </h1>
+            <Table data = {students}/>
+        </div>
+    )
+};
+export default App;
 
 
+const students = [
+    {
+        name: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 85
+    },
+    {
+        name: 'Bruce Banner',
+        course: 'Psychology',
+        grade: 91
+    },
+    {
+        name: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: 100
+    }
+];
