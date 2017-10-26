8d46a6b8c71fbfef2a004eb7bfc85fd6cac35d1d
pk316 <pr.khiev@gmail.com>
2017-10-26 11:13:44 -0700

    forms-inputs

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        firstName : 'Steve',
+        lastName : 'Richardson',
+        phone : '7142849475',
+        email : 'richardson@gmail.com'
+    },
+    {
+        firstName : 'David',
+        lastName : 'Johnson',
+        phone : '31046587655',
+        email : 'djohnson@gmail.com'
+    },
+    {
+        firstName : 'Sam',
+        lastName : 'Taylor',
+        phone : '2134565432',
+        email : 'STay@hotmail.com'
+    }
+];
