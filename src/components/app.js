adfc75316b14f7ac3cb1b35c3d0ad14d3a906191
pk316 <pr.khiev@gmail.com>
2017-11-01 20:39:36 -0700

    redux-intro

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import Clock from './clock';
 
+export default () =>(
+    <div className="container">
+        <h1 className="text-center">This is a Clock!</h1>
+    <Clock/>
+    </div>
+)
 
